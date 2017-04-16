4b800c51f507899c4d9569ba315173f573e87114
bellazhai <yiluzouhao168@126.com>
2017-04-16 19:45:27 +0800

    ajdf

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'jobs#index'
 
+    namespace :admin do
+      resources :jobs
+    end
+
 
 end
