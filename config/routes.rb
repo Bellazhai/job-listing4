0dff1a051bdfd6660cd76da65c52adc8c9478713
bellazhai <yiluzouhao168@126.com>
2017-04-17 00:37:17 +0800

    dai

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-    resources :jobs
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root 'jobs#index'
+
 
     namespace :admin do
       resources :jobs do
@@ -12,6 +12,14 @@ Rails.application.routes.draw do
         end
     end
   end
+
+  resources :jobs do
+    resources :resumes
+  end
+
+
+     root 'jobs#index'
+
 
 
 end
